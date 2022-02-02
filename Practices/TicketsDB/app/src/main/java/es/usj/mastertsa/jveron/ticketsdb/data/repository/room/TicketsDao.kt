6fae406d7a4f75dfe708170f98bf708e65a34ff2
Jorge Chueca <jchueca_96@hotmail.com>
2022-02-02 20:20:40 +0100

    Dao and Database

--- Practices/TicketsDB/app/src/main/java/es/usj/mastertsa/jveron/ticketsdb/data/repository/room/TicketsDao.kt
+++ Practices/TicketsDB/app/src/main/java/es/usj/mastertsa/jveron/ticketsdb/data/repository/room/TicketsDao.kt
@@ -1,4 +1,26 @@
 package es.usj.mastertsa.jveron.ticketsdb.data.repository.room
 
-class TicketsDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TicketsDao {
+    
+    @Insert
+    suspend fun insertUser(userDbModel: UserDbModel)
+    
+    @Query("SELECT * FROM $USERS_TABLE_NAME WHERE email = :email")
+    fun getUser(email: String): Flow<UserDbModel>
+    
+    @Insert
+    suspend fun insertEvent(sightDbModel: EventDbModel)
+    
+    @Query("SELECT * FROM $EVENTS_TABLE_NAME")
+    suspend fun getEvents(): Flow<List<EventDbModel>>
+    
+    @Query("SELECT * FROM $USERS_TABLE_NAME WHERE user_id = :userId")
+    suspend fun getUserAndEvents(userId: Int): UserWithEventsDbModel
+    
 }
--- Practices/TicketsDB/app/src/main/java/es/usj/mastertsa/jveron/ticketsdb/data/repository/room/TicketsDatabase.kt
+++ Practices/TicketsDB/app/src/main/java/es/usj/mastertsa/jveron/ticketsdb/data/repository/room/TicketsDatabase.kt
@@ -1,4 +1,30 @@
 package es.usj.mastertsa.jveron.ticketsdb.data.repository.room
 
-class TicketsDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [UserDbModel::class, EventDbModel::class], version = 1, exportSchema = true)
+abstract class TicketsDatabase: RoomDatabase() {
+    
+    abstract fun getDao(): TicketsDao
+    
+    companion object {
+    
+        @Volatile
+        private var INSTANCE : TicketsDatabase? = null
+    
+        fun getDatabase(context: Context): TicketsDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context,
+                    TicketsDatabase::class.java,
+                    "TicketsRoom.db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
