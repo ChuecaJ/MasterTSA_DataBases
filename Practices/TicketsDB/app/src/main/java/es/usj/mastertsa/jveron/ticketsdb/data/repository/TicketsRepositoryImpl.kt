cd784c8e9c78f1d6803d3193d6dda8507ad6f0b7
Jorge Chueca <jchueca_96@hotmail.com>
2022-02-02 20:25:39 +0100

    Started Repository Impl

--- Practices/TicketsDB/app/src/main/java/es/usj/mastertsa/jveron/ticketsdb/data/repository/TicketsRepositoryImpl.kt
+++ Practices/TicketsDB/app/src/main/java/es/usj/mastertsa/jveron/ticketsdb/data/repository/TicketsRepositoryImpl.kt
@@ -1,4 +1,29 @@
 package es.usj.mastertsa.jveron.ticketsdb.data.repository
 
-class TicketsRepositoryImpl {
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import es.usj.mastertsa.jveron.ticketsdb.data.repository.api.EventsService
+import es.usj.mastertsa.jveron.ticketsdb.data.repository.room.TicketsDao
+import es.usj.mastertsa.jveron.ticketsdb.domain.model.Event
+import es.usj.mastertsa.jveron.ticketsdb.domain.model.User
+import es.usj.mastertsa.jveron.ticketsdb.domain.model.UserAndEvents
+import es.usj.mastertsa.jveron.ticketsdb.domain.repository.TicketsRepository
+import kotlinx.coroutines.flow.Flow
+
+class TicketsRepositoryImpl(
+    private val dataStore: DataStore<Preferences>,
+    private val eventsService: EventsService,
+    private val TicketsDao: TicketsDao
+): TicketsRepository {
+    override suspend fun getEvents(): Flow<List<Event>> {
+        TODO("Not yet implemented")
+    }
+    
+    override suspend fun getUserAndEvents(userId: Int): UserAndEvents {
+        TODO("Not yet implemented")
+    }
+    
+    override suspend fun getUser(email: String): User {
+        TODO("Not yet implemented")
+    }
 }
