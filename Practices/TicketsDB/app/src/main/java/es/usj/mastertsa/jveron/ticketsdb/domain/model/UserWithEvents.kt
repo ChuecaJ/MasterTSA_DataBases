cf24b39a50cecc4da000d2f752bb50f1b7b93834
veronlinks <verontavasco@gmail.com>
2022-02-03 10:28:24 +0100

    advances

--- Practices/TicketsDB/app/src/main/java/es/usj/mastertsa/jveron/ticketsdb/domain/model/UserWithEvents.kt
+++ Practices/TicketsDB/app/src/main/java/es/usj/mastertsa/jveron/ticketsdb/domain/model/UserWithEvents.kt
@@ -1,6 +1,6 @@
 package es.usj.mastertsa.jveron.ticketsdb.domain.model
 
-data class UserAndEvents (
+data class UserWithEvents (
     val user: User,
     val events: List<Event>
     )
--- Practices/TicketsDB/app/src/main/java/es/usj/mastertsa/jveron/ticketsdb/domain/repository/TicketsRepository.kt
+++ Practices/TicketsDB/app/src/main/java/es/usj/mastertsa/jveron/ticketsdb/domain/repository/TicketsRepository.kt
@@ -2,14 +2,14 @@ package es.usj.mastertsa.jveron.ticketsdb.domain.repository
 
 import es.usj.mastertsa.jveron.ticketsdb.domain.model.Event
 import es.usj.mastertsa.jveron.ticketsdb.domain.model.User
-import es.usj.mastertsa.jveron.ticketsdb.domain.model.UserAndEvents
+import es.usj.mastertsa.jveron.ticketsdb.domain.model.UserWithEvents
 import kotlinx.coroutines.flow.Flow
 
 interface TicketsRepository {
 
     suspend fun getEvents(): Flow<List<Event>>
 
-    suspend fun getUserAndEvents(userId: Int): UserAndEvents
+    suspend fun getUserAndEvents(userId: Int): UserWithEvents
 
     suspend fun getUser(email: String): User
 
