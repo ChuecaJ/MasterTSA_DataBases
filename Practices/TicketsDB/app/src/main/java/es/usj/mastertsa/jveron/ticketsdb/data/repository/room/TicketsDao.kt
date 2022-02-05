d6216f42601e91919891c84843f670a59848274a
Jorge Chueca <jchueca_96@hotmail.com>
2022-02-05 11:53:25 +0100

    Dao GetEventById

--- Practices/TicketsDB/app/src/main/java/es/usj/mastertsa/jveron/ticketsdb/data/repository/room/TicketsDao.kt
+++ Practices/TicketsDB/app/src/main/java/es/usj/mastertsa/jveron/ticketsdb/data/repository/room/TicketsDao.kt
@@ -8,18 +8,24 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface TicketsDao {
     
+    // Users
     @Insert
     suspend fun insertUser(userDbModel: UserDbModel)
     
     @Query("SELECT * FROM $USERS_TABLE_NAME WHERE user_email = :email")
     fun getUser(email: String): Flow<UserDbModel>
     
+    // Events
     @Insert
     suspend fun insertEvent(sightDbModel: EventDbModel)
     
     @Query("SELECT * FROM $EVENTS_TABLE_NAME")
     fun getEvents(): Flow<List<EventDbModel>>
     
+    @Query("SELECT * FROM $EVENTS_TABLE_NAME WHERE event_id = :id")
+    fun getEventById(id: Int): Flow<EventDbModel>
+    
+    // UserAndEvents
     @Query("SELECT * FROM $USERS_TABLE_NAME WHERE user_email = :email")
     suspend fun getUserAndEvents(email: String): UserWithEventsDbModel
     
